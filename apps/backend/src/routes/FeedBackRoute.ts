11ee387cf9ba10fb9304a599c00c0ede51eadf4f
Katy Stuparu <kastuparu@wpi.edu>
2024-03-29 14:37:11 -0400

    added prisma, for anyone who is interested

--- apps/backend/src/routes/FeedBackRoute.ts
+++ apps/backend/src/routes/FeedBackRoute.ts
@@ -1,30 +1,46 @@
-import express, { Router } from "express";
-import { FeedBack } from "common/src/Feedback.ts";
+import express, { Router, Request, Response } from "express";
+import client from "../bin/database-connection.ts";
+import { FeedBack } from "database";
 
 const router: Router = express.Router();
 
-const database: FeedBack[] = [];
+// const database: FeedBack[] = [];
 
-router.post("/", (req, res) => {
-  const feedback: FeedBack = req.body as FeedBack;
-  database.push(feedback);
-  console.log(database);
-  res.status(200).json({ message: "success" });
-});
-
-router.get("/", (req, res) => {
-  res.json(database);
-});
-
-router.get("/:index", (req, res) => {
-  const index = parseInt(req.params.index);
-  if (isNaN(index)) {
-    return res.status(400).json({ message: "not a number" });
+router.post("/", async function (req: Request, res: Response): Promise<void> {
+  const feedback: FeedBack = req.body;
+  try {
+    await client.feedBack.create({
+      data: feedback,
+    });
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "error" });
   }
-  if (index < 0 || index >= database.length) {
-    return res.status(400).json({ message: "out of bounds" });
-  }
-  res.json(database[index]);
 });
+
+router.get("/", async function (req: Request, res: Response): Promise<void> {
+  const feedback: FeedBack[] = await client.feedBack.findMany();
+  res.json(feedback);
+});
+
+router.get(
+  "/:index",
+  async function (req: Request, res: Response): Promise<void> {
+    const index = parseInt(req.params.index);
+    const feedback: FeedBack | null = await client.feedBack.findUnique({
+      where: {
+        id: index,
+      },
+    });
+    if (feedback !== null) res.json(feedback);
+    else {
+      console.error("could not find feedback with id " + index);
+      res
+        .status(400)
+        .json({ message: "could not find feedback with id " + index });
+    }
+  },
+);
 
 export default router;
